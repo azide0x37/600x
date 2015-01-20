b53f66e10ca5149e129db7ef63c7db5441b5c59f
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-01-20 01:45:53 -0600

    added final totals and corrected the monthCount

--- credit_min.py
+++ credit_min.py
@@ -2,17 +2,20 @@ __author__ = 'Alexander'
 balance = 5000
 annualInterestRate = 0.18
 monthlyPaymentRate = 0.02
-
 monthCount=1
 unpaidBalance = balance
 rate = annualInterestRate / 12
 currentPayment = 0
+totalPayments = 0
 
-while(monthCount < 12):
-	currentPayment = (unpaidBalance * monthlyPaymentRate)  
-	unpaidBalance -= currentPayment
-	unpaidBalance = (unpaidBalance * rate) + unpaidBalance
-	print "Month:", monthCount
-	print "Minimum monthly payment:", round(currentPayment,2) 
-	print "Remaining balance:", round(unpaidBalance,2)
-	monthCount += 1
+while(monthCount < 13):
+    currentPayment = (unpaidBalance * monthlyPaymentRate) 
+    totalPayments += currentPayment
+    unpaidBalance -= currentPayment
+    unpaidBalance = (unpaidBalance * rate) + unpaidBalance
+    print "Month:", monthCount
+    print "Minimum monthly payment:", round(currentPayment,2) 
+    print "Remaining balance:", round(unpaidBalance,2)
+    monthCount += 1
+print "Total Paid:", round(totalPayments,2)
+print "Remaining Balance:", round(unpaidBalance,2)
