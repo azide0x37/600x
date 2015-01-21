26705e5a4c841cbe78b37c096238585abf9760ad
Alexander Templeton <blakmatr@gmail.com>
2015-01-21 00:15:02 -0600

    Some code cleanup, added printouts for debugging

--- fixedPayment.py
+++ fixedPayment.py
@@ -1,26 +1,25 @@
-balance 3929
+balance = 3929
 annualInterestRate = 0.2
 monthlyInterestRate = annualInterestRate / 12
 lowPayment = 0
 
 newBalance = balance
-months = 0
 cycles = 0
 
+balance = int(raw_input('enter a new balance: '))
 while newBalance > 0:
-	newBalance = balance
 	lowPayment += 10
-	#print "Cycle:", cycles
+	newBalance = balance
+	print "Cycle:", cycles
 	
-	while months < 12:
+	for months in range(0,12):
 		newBalance = newBalance + (newBalance * monthlyInterestRate) - lowPayment
-		#print "Month:", months, "new balance", round(newBalance, 2)
+		print "Month:", months, "new balance", round(newBalance, 2)
 		months += 1
 	
-	#print "Low payment:", lowPayment
-	
+	print "Low payment:", lowPayment
 	months = 0
 	cycles += 1
 
-print "Lowest payment", round(lowPayment, 2)
+print "Lowest payment:", round(lowPayment, 2)
 
